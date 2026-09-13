d9c5f7b58e36d60e2212afcf7033169a293419ff
Cheng <chiacheng.tsao@gmail.com>
2026-09-14 02:48:11 +0800

    feat: export SMB1 M1 contracts

--- src/fami_pixel/games/smb1/__init__.py
+++ src/fami_pixel/games/smb1/__init__.py
@@ -1,5 +1,7 @@
-"""Super Mario Bros. game-specific state decoding."""
+"""Super Mario Bros. game-specific decoding and M1 environment contracts."""
 
+from .actions import ActionCommand, Smb1Action, action_to_nes_buttons
+from .observation import Smb1Observation, observation_from_state, read_smb1_observation
 from .state import (
     GAME_MODE,
     PLAYER_CONTROL_SUBROUTINE,
@@ -9,9 +11,15 @@ from .state import (
 )
 
 __all__ = [
+    "ActionCommand",
     "GAME_MODE",
     "PLAYER_CONTROL_SUBROUTINE",
+    "Smb1Action",
+    "Smb1Observation",
     "TITLE_SCREEN_MODE",
     "Smb1State",
+    "action_to_nes_buttons",
+    "observation_from_state",
+    "read_smb1_observation",
     "read_smb1_state",
 ]
