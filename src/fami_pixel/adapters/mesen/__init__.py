05f3f7be85bebf701ebd5679664112374c93d749
Cheng <chiacheng.tsao@gmail.com>
2026-09-13 23:35:19 +0800

    feat: export headless NES controller configuration helpers

--- src/fami_pixel/adapters/mesen/__init__.py
+++ src/fami_pixel/adapters/mesen/__init__.py
@@ -1,5 +1,14 @@
 """Mesen CE adapter boundary."""
 
+from .config import (
+    CONTROLLER_TYPE_NES_CONTROLLER,
+    ControllerConfig,
+    KeyMapping,
+    KeyMappingSet,
+    NesConfig,
+    configure_standard_nes_controller,
+    get_nes_config,
+)
 from .controller import (
     DebugControllerState,
     available_input_overrides,
@@ -10,10 +19,17 @@ from .controller import (
 from .loader import MesenCore, MesenLoadError
 
 __all__ = [
+    "CONTROLLER_TYPE_NES_CONTROLLER",
+    "ControllerConfig",
     "DebugControllerState",
+    "KeyMapping",
+    "KeyMappingSet",
     "MesenCore",
     "MesenLoadError",
+    "NesConfig",
     "available_input_overrides",
+    "configure_standard_nes_controller",
+    "get_nes_config",
     "released_state",
     "right_state",
     "set_input_override",
