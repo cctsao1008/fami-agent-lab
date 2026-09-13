18f295e6c639baf2137f121cea366fa413af472e
Cheng <chiacheng.tsao@gmail.com>
2026-09-13 23:43:47 +0800

    feat: export Mesen native memory read helpers

--- src/fami_pixel/adapters/mesen/__init__.py
+++ src/fami_pixel/adapters/mesen/__init__.py
@@ -17,6 +17,14 @@ from .controller import (
     set_input_override,
 )
 from .loader import MesenCore, MesenLoadError
+from .memory import (
+    MEMORY_TYPE_NES_INTERNAL_RAM,
+    MEMORY_TYPE_NES_MEMORY,
+    get_memory_size,
+    read_memory_value,
+    read_nes_cpu_memory,
+    read_nes_internal_ram,
+)
 
 __all__ = [
     "CONTROLLER_TYPE_NES_CONTROLLER",
@@ -24,12 +32,18 @@ __all__ = [
     "DebugControllerState",
     "KeyMapping",
     "KeyMappingSet",
+    "MEMORY_TYPE_NES_INTERNAL_RAM",
+    "MEMORY_TYPE_NES_MEMORY",
     "MesenCore",
     "MesenLoadError",
     "NesConfig",
     "available_input_overrides",
     "configure_standard_nes_controller",
+    "get_memory_size",
     "get_nes_config",
+    "read_memory_value",
+    "read_nes_cpu_memory",
+    "read_nes_internal_ram",
     "released_state",
     "right_state",
     "set_input_override",
